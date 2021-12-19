4927779a3c98e60e40c854af0d62158ee1683928
AhmedLabibb <92722943+AhmedLabibb@users.noreply.github.com>
2021-12-19 11:50:07 +0400

    Initial Commit

---
+++ src/main/java/com/tappayment/api/TapPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.tappayment.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapPaymentApiApplication.class, args);
+	}
+
+}
